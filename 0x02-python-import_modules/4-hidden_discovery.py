e20c84373cab8b2d86cb753179e6dd07b7a188ca
timothyVoca <vocalync@gmail.com>
2023-06-18 11:39:51 +0000

    fixed comments

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-'''
-Prints all names defined by the compiled module
-should not execute when importedodule hidden_4.pyc
-Print one name per line in alpha order
-Print only names that do not start with __
-Your code should not execute when imported
-'''
-
 if __name__ == "__main__":
     import hidden_4
 
