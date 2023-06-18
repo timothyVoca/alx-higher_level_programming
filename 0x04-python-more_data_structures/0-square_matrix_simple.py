1248aef91fd848152e0f429ab8bfeded469ed79f
timothyVoca <vocalync@gmail.com>
2023-06-18 18:20:19 +0000

    another fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# 3-print_reversed_list_integer.py
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
 
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-def print_reversed_list_integer(my_list=[]):
-    """Print all integers of a list in reverse order."""
-    if isinstance(my_list, list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+    return (new_matrix)
